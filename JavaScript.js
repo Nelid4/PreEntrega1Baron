b9a83205ff7159886df3138c35c026dc6aa11e64
Neli <nelidabaron@gmail.com>
2023-06-03 19:46:42 -0300

    pruebaa

---
+++ JavaScript.js
@@ -0,0 +1,63 @@
+let nombreIngresado = prompt("Escribí tu nombre.")
+let pregunta = prompt(`Hola ${nombreIngresado}, bienvenidx a nuestro menú`)
+console.log(pregunta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function venderLibro() {
+//     const libro = prompt("¿Qué libro quieres vender? ");
+//     const precio = prompt("¿A cuánto lo vas a vender? ");
+//     console.log("Has ingresado el libro: " + libro + " a un precio de " + precio + " dólares.");
+// }
+
+// function comprarLibro() {
+//     console.log("No hay libros en stock.");
+// }
+
+// function guardarLibro() {
+//     const nombre = prompt("Ingresa el nombre del libro: ");
+//     const autor = prompt("Ingresa el autor del libro: ");
+//     const calificacion = prompt("¿Cuánto te gustó el libro? (1 al 5): ");
+//     console.log("Has guardado el libro: " + nombre + " del autor " + autor + " con una calificación de " + calificacion + ".");
+// }
+
+// // Menú principal
+// let opcion = "";
+// while (opcion !== "salir") {
+//     opcion = prompt("¿Qué deseas hacer? (vender, comprar, guardar, salir): ");
+    
+//     switch (opcion) {
+//         case "vender":
+//             venderLibro();
+//             break;
+//         case "comprar":
+//             comprarLibro();
+//             break;
+//         case "guardar":
+//             guardarLibro();
+//             break;
+//         case "salir":
+//             console.log("Saliendo del programa.");
+//             break;
+//         default:
+//             console.log("Opción inválida. Por favor, intenta nuevamente.");
+//             break;
+//     }
+// }
